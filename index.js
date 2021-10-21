fb48b32704a3407c0004fc5843b7cfac0ed6df15
Greysonnn <25331809+Greysonnn@users.noreply.github.com>
2021-10-20 19:38:28 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,46 @@
-let firstCard = 11
-let secondCard = 10
-let cards = [firstCard, secondCard]
-let sum = firstCard + secondCard
+//
+//
+let cards = []
+let sum = 0
 let hasBlackJack = false
+let isAlive = false
 let busted = true
 let message = ''
 let messageEl = document.getElementById('message-el')
 let sumEl = document.querySelector('#sum-el')
 let cardsEl = document.querySelector('#cards-el')
 
+console.log(cards)
+
+function getRandomCard() {
+
+    let randomCard = Math.floor(Math.random() * 13) + 1
+    if (randomCard > 10)  {
+        return 10
+    } else if (randomCard === 1) {
+        return 11
+    } else {
+        return randomCard
+    }
+}
+
 function startGame() {
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
     renderGame()
 }
 
 function renderGame() {
-    cardsEl.textContent = "Cards: " + cards[0] + ', ' + cards[1]
+    cardsEl.textContent = "Cards: "
+    for (let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i] + ", "
+    }
     sumEl.textContent = 'Sum: ' + sum
     if (sum <= 20) {
-        message = 'Do you want to hit?'
-         
+        message = 'Do you want another card?'
      } else if (sum === 21) {
          message = "You've got Blackjack!"
          hasBlackJack = true
@@ -30,13 +52,10 @@ function renderGame() {
 }
 
 function newCard() {
-    console.log('Drawing new card from deck')
-    let card = 3
+    console.log('Drawing new card from deck') //for testing purposes
+    let card = getRandomCard()
     sum += card
     cards.push(card)
     console.log(cards)
     renderGame()
 }
-
-// Cash out
-
